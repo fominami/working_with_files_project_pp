d6459335635364571ea846e72861ce07a9a70bc6
Maria Fomina <6002maria@gmail.com>
2024-12-05 19:18:18 +0300

    change library for data processing

--- mainproject/mainapp/utils/data_processing.py
+++ mainproject/mainapp/utils/data_processing.py
@@ -1,20 +1,28 @@
-import re 
-from sympy import sympify, SympifyError
+import re
+import numexpr as ne
 
 class DataProcessor:
-    def __init__(self,data):
-        self.data=data
-    def extract_math_expressions(self, data): 
-        # Используем регулярные выражения для извлечения математических выражений 
-        pattern = re.compile(r'(\d+\s*[\+\-\*/]\s*\d+)') 
+    def __init__(self, data):
+        self.data = data
+
+    def extract_math_expressions(self, data):
+        pattern = re.compile(r'(\b\d+(\.\d+)?(?:[\+\-\*/\(\)])*\d+(\.\d+)?\b)')
         return pattern.findall(data)
-    def process_with_library(self): 
-        math_expressions = self.extract_math_expressions(self.data) 
-        for expr in math_expressions:
-            try:
-                result = sympify(expr) 
-                self.data = self.data.replace(expr, str(result)) 
-            except SympifyError: 
-                pass
-            
+
+    def process_with_library(self):
+        while True:
+            initial_data = self.data
+            math_expressions = self.extract_math_expressions(self.data)
+
+            for expr_tuple in math_expressions:
+                expr = expr_tuple[0]
+                try:
+                    result = ne.evaluate(expr)
+                    self.data = self.data.replace(expr, str(result))
+                except Exception as e:
+                    print(f"Error processing expression '{expr}': {e}")
+                    pass  
+            if self.data == initial_data:
+                break
+
         return self.data
